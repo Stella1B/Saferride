d316f2be86cde81c47e3959752247f310511d7c8
admin@joy.com <jdricilla@gmail.com>
2024-08-04 11:47:15 +0300

    profile details when logging in

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: unused_import
 
-import 'package:boda/main.dart';
 import 'package:boda/screens/contact_us_page.dart';
 import 'package:boda/screens/family%20button.dart';
+import 'package:boda/screens/navigation_screen.dart';
 import 'package:boda/screens/notifications.dart';
 import 'package:boda/screens/promotions.dart';
 import 'package:flutter/foundation.dart';
@@ -13,9 +13,10 @@ import 'package:geolocator/geolocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:boda/screens/sign_up.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -32,30 +33,65 @@ class _HomeScreenState extends State<HomeScreen> {
   LatLng? _curLocation;
   bool _locationLoaded = false;
   List<LatLng> _routePoints = [];
-
-  get screens => null;
+  bool _isLoggedIn = false;
 
   @override
   void initState() {
     super.initState();
-    _loadProfileData();
-    _getCurrentLocation();
-    _loadRoutePoints();
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      setState(() {
+        _isLoggedIn = true;
+      });
+
+      await _loadProfileData();
+      await _getCurrentLocation();
+      await _loadRoutePoints();
+    } else {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const SignUpPage()));
+    }
   }
 
   Future<void> _loadProfileData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String name = prefs.getString('name') ?? '';
-    String number = prefs.getString('number') ?? '';
-    String nextOfKin = prefs.getString('nextOfKin') ?? '';
-    String nextOfKinContact = prefs.getString('nextOfKinContact') ?? '';
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return;
 
-    setState(() {
-      _userName = name;
-      _userNumber = number;
-      _nextOfKin = nextOfKin;
-      _nextOfKinContact = nextOfKinContact;
-    });
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? name = prefs.getString('name');
+    String? number = prefs.getString('number');
+    String? nextOfKin = prefs.getString('nextOfKin');
+    String? nextOfKinContact = prefs.getString('nextOfKinContact');
+
+    if (name != null && number != null && nextOfKin != null && nextOfKinContact != null) {
+      setState(() {
+        _userName = name;
+        _userNumber = number;
+        _nextOfKin = nextOfKin;
+        _nextOfKinContact = nextOfKinContact;
+      });
+    } else {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+      if (userDoc.exists) {
+        final data = userDoc.data() as Map<String, dynamic>;
+        setState(() {
+          _userName = data['name'];
+          _userNumber = data['number'];
+          _nextOfKin = data['nextOfKin'];
+          _nextOfKinContact = data['nextOfKinContact'];
+        });
+
+        await prefs.setString('name', _userName);
+        await prefs.setString('number', _userNumber);
+        await prefs.setString('nextOfKin', _nextOfKin);
+        await prefs.setString('nextOfKinContact', _nextOfKinContact);
+      }
+    }
   }
 
   Future<void> _getCurrentLocation() async {
@@ -64,7 +100,7 @@ class _HomeScreenState extends State<HomeScreen> {
           desiredAccuracy: LocationAccuracy.high);
       setState(() {
         _curLocation = LatLng(position.latitude, position.longitude);
-        _locationLoaded = true; // Set the flag to true when location is loaded
+        _locationLoaded = true;
       });
     } catch (e) {
       print('Failed to get current location: $e');
@@ -94,8 +130,7 @@ class _HomeScreenState extends State<HomeScreen> {
     });
 
     final response = await http.post(url, headers: headers, body: body);
-    print(response.statusCode);
-    print('$response');
+    print('Distress Signal Response: ${response.statusCode}');
   }
 
   void _shareLocationWithSafeBoda(LatLng location) {
@@ -121,6 +156,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> _logout() async {
+    await FirebaseAuth.instance.signOut();
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.clear();
 
@@ -137,7 +173,7 @@ class _HomeScreenState extends State<HomeScreen> {
           IconButton(
             icon: const Icon(Icons.warning),
             color: const Color.fromARGB(255, 167, 10, 10),
-            onPressed: () => _sendDistressSignal(_curLocation!),
+            onPressed: _curLocation != null ? () => _sendDistressSignal(_curLocation!) : null,
           ),
           IconButton(
             icon: const Icon(Icons.logout),
@@ -145,79 +181,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text(_userName),
-              currentAccountPicture: InkWell(
-                onTap: () => _showProfileDialog(context),
-                child: const CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.person, size: 50, color: Colors.green),
-                ),
-              ),
-              accountEmail: null,
-            ),
-            _buildDrawerItem(
-              icon: Icons.card_giftcard,
-              title: 'Promotions',
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const PromotionsScreen()));
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.scanner,
-              title: 'Scan code',
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ScanCodePage()));
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.notifications,
-              title: 'Notifications',
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const NotificationsPage()));
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.contact_emergency_rounded,
-              title: 'Contact Us',
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ContactUsPage()));
-              },
-            ),
-            const Divider(),
-            _buildDrawerItem(
-              icon: Icons.people,
-              title: 'Family Button',
-              iconColor: const Color.fromARGB(255, 208, 211, 10),
-              textColor: const Color.fromARGB(255, 40, 42, 44),
-              onTap: () {
-                _showFamilyDialog(context);
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.logout,
-              title: 'Logout',
-              onTap: _logout,
-            ),
-          ],
-        ),
-      ),
+      drawer: _buildDrawer(),
       body: _locationLoaded
           ? NavigationScreen(
               lat: _curLocation!.latitude,
@@ -227,12 +191,87 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _buildDrawerItem(
-      {required IconData icon,
-      required String title,
-      required VoidCallback onTap,
-      Color iconColor = Colors.black54,
-      Color textColor = Colors.black87}) {
+  Widget _buildDrawer() {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text(_userName),
+            currentAccountPicture: InkWell(
+              onTap: () => _showProfileDialog(context),
+              child: const CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(Icons.person, size: 50, color: Colors.green),
+              ),
+            ),
+            accountEmail: null,
+          ),
+          _buildDrawerItem(
+            icon: Icons.card_giftcard,
+            title: 'Promotions',
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const PromotionsScreen()));
+            },
+          ),
+          _buildDrawerItem(
+            icon: Icons.scanner,
+            title: 'Scan code',
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ScanCodePage()));
+            },
+          ),
+          _buildDrawerItem(
+            icon: Icons.notifications,
+            title: 'Notifications',
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const NotificationsPage()));
+            },
+          ),
+          _buildDrawerItem(
+            icon: Icons.contact_emergency_rounded,
+            title: 'Contact Us',
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ContactUsPage()));
+            },
+          ),
+          const Divider(),
+          _buildDrawerItem(
+            icon: Icons.people,
+            title: 'Family Button',
+            iconColor: const Color.fromARGB(255, 208, 211, 10),
+            textColor: const Color.fromARGB(255, 40, 42, 44),
+            onTap: () => _showFamilyDialog(context),
+          ),
+          _buildDrawerItem(
+            icon: Icons.logout,
+            title: 'Logout',
+            onTap: _logout,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDrawerItem({
+    required IconData icon,
+    required String title,
+    required VoidCallback onTap,
+    Color iconColor = Colors.black54,
+    Color textColor = Colors.black87,
+  }) {
     return ListTile(
       leading: Icon(icon, color: iconColor),
       title: Text(title, style: TextStyle(color: textColor)),
@@ -258,14 +297,14 @@ class _HomeScreenState extends State<HomeScreen> {
               Text('Next of Kin: $_nextOfKin',
                   style: const TextStyle(fontSize: 16)),
               const SizedBox(height: 10),
-              Text("Next of Kin's Contact: $_nextOfKinContact",
+              Text('Next of Kin Contact: $_nextOfKinContact',
                   style: const TextStyle(fontSize: 16)),
             ],
           ),
           actions: [
             TextButton(
-              child: const Text('Close'),
               onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Close'),
             ),
           ],
         );
@@ -278,97 +317,16 @@ class _HomeScreenState extends State<HomeScreen> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Family Button Pressed'),
-          content: const Text('Are you sure you want to share ride details?'),
+          title: const Text('Family Button'),
+          content: const Text('Family Button details go here.'),
           actions: [
             TextButton(
-              child: const Text('Cancel'),
               onPressed: () => Navigator.of(context).pop(),
-            ),
-            TextButton(
-              child: const Text('Confirm'),
-              onPressed: () {
-                Navigator.of(context).pop();
-                _shareRideDetails();
-              },
+              child: const Text('Close'),
             ),
           ],
         );
       },
     );
-  }
-
-  void _shareRideDetails() {
-    NextOfKin nextOfKin = NextOfKin(name: 'John Doe', phone: '+256786230754');
-    Client client =
-        Client(name: _userName, phone: _userNumber, nextOfKin: nextOfKin);
-    Rider rider = Rider(
-        name: 'Mike Johnson',
-        phone: '+1122334455',
-        bikeDetails: 'Honda CBR 250R');
-
-    matchRiderToClient(client, rider);
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Ride details shared successfully')),
-    );
-  }
-}
-
-class NextOfKin {
-  final String name;
-  final String phone;
-
-  NextOfKin({required this.name, required this.phone});
-}
-
-class Client {
-  final String name;
-  final String phone;
-  final NextOfKin nextOfKin;
-
-  Client({required this.name, required this.phone, required this.nextOfKin});
-}
-
-class Rider {
-  final String name;
-  final String phone;
-  final String bikeDetails;
-
-  Rider({required this.name, required this.phone, required this.bikeDetails});
-}
-
-void matchRiderToClient(Client client, Rider rider) {
-  // print('Matching ${rider.name} to ${client.name}');
-  // sendMessageToNextOfKin(client.nextOfKin, rider);
-  
-  print("Delivered");
-}
-
-void sendMessageToNextOfKin(NextOfKin nextOfKin, Rider rider) async {
-  Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high);
-  String locationUrl =
-      "https://www.google.com/maps/search/?api=1&query=${position.latitude},${position.longitude}";
-
-  String message = 'Dear ${nextOfKin.name},\n\n'
-      'Your relative has been matched with a rider. Here are the rider details:\n'
-      'Name: ${rider.name}\n'
-      'Phone: ${rider.phone}\n'
-      'Bike: ${rider.bikeDetails}\n\n'
-      'Current location: $locationUrl';
-
-  await sendWhatsAppMessage(nextOfKin.phone, message);
-}
-
-Future<void> sendWhatsAppMessage(String phone, String message) async {
-  String encodedMessage = Uri.encodeComponent(message);
-  String whatsappUrl =
-      "https://wa.me/${phone.replaceAll('+', '')}?text=$encodedMessage";
-
-  if (await canLaunch(whatsappUrl)) {
-    await launch(whatsappUrl);
-  } else {
-    print('Could not launch WhatsApp. URL: $whatsappUrl');
   }
 }
